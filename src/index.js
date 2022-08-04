f0ac39520d6f954d3e9532670b1e3eeecf678add
MrBugatti <bellino.brian@gmail.com>
2022-08-04 15:47:25 -0400

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import _ from 'lodash';
+
+// function component() {
+//     const element = document.createElement('div');
+//     return element;
+// }
+
+// document.body.appendChild(component());
